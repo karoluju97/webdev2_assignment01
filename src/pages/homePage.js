1506d192486256891c5cb3d395b2011fe5c403fd
Karol Uju Amamjirionwu <a.karoluju97@gmail.com>
2020-11-04 00:21:50 +0000

    Add to favorites logic completed; Dummy favorites page.

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
 import Header from "../components/headerMovieList";
 import MovieList from "../components/movieList";
 import FilterControls from "../components/filterControls";
@@ -40,18 +40,21 @@ const MovieListPage = () => {
     const addToFavorites = movieId => {
         // Find index position of selected movie in the list
         const index = movies.map(m => m.id).indexOf(movieId)
-    
+
         StubAPI.add(movies[index])
         const updatedList = [...movies]  // Make a copy of the movie list
         updatedList.splice(index, 1)     // Remove selected movie from home page list
-        setMovies(updatedList)  
-      }
+        setMovies(updatedList)
+    }
 
     return (
         <>
-            <Header numMovies={displayedMovies.length} />          {/* CHANGED */}
-            <FilterControls onUserInput={handleFilterChange} />    {/* CHANGED */}
-            <MovieList movies={displayedMovies} />                  {/* CHANGED */}
+            <Header numMovies={displayedMovies.length} />
+            <FilterControls onUserInput={handleFilterChange} />
+            <MovieList
+                movies={displayedMovies}
+                buttonHandler={addToFavorites}
+            />     {/* CHANGED */}
         </>
     );
 };
--- src/pages/movieDetailsPage.js
+++ src/pages/movieDetailsPage.js
@@ -1,5 +1,4 @@
-import React from "react";
-import React, {useState, useEffect}  from "react";
+import React, {useState, useEffect } from "react";
 import MovieHeader from "../components/headerMovie";
 import MovieDetails from "../components/movieDetails";
 import "./moviePage.css";
