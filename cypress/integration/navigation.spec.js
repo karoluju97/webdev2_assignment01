d24f6cf155954378108f2a1240190da69c723c73
Karol Uju Amamjirionwu <a.karoluju97@gmail.com>
2020-11-06 16:52:28 +0000

    E2E tests - Navigation - Movies Details page Back button

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -73,4 +73,18 @@ describe("Navigation", () => {
       cy.get("h2").contains(movies[0].title);
     });
   });
+  describe("The Go Back button", () => {
+    beforeEach(() => {
+      cy.visit("/");
+    });
+    it("should navigate from home page to movie details and back", () => {
+      cy.get(".card").eq(1).find("img").click();
+      cy.get("svg[data-icon=arrow-circle-left]").click();
+      cy.url().should("not.include", `/movies`);
+      cy.get("h2").contains("No. Movies");
+    });
+    it("should navigate from favorites page to movie details and back", () => {
+      // TODO
+    });
+  });
 });
--- src/components/headerMovieList/index.js
+++ src/components/headerMovieList/index.js
@@ -1,16 +1,27 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { withRouter } from "react-router-dom";
 
-const Header = ({ title, numMovies }) => {
+const MovieHeader = ({ movie, history }) => {
   return (
     <div className="row">
-      <div className="col-md-6 offset-4">
+             <div className="col-2">
+          <button onClick={() => history.goBack()}>
+            <FontAwesomeIcon icon={["fas", "arrow-circle-left"]} size="2x" />
+            <span>{" Back"}</span>
+          </button>
+        </div>
+      <div className="col-6 offset-3">
         <h2>
-          {`${title}  `}
-          <span className="badge badge-pill badge-success">{numMovies}</span>
+          {movie.title}
+          {"  "}
+          <a href={movie.homepage}>
+            <FontAwesomeIcon icon={["fas", "home"]} size="1x" />
+          </a>
         </h2>
       </div>
     </div>
   );
 };
 
-export default Header;
+export default withRouter( MovieHeader );
