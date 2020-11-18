55b5f6be00bc3c1d8dabe39c1e157a1b9c6c444f
Karol Uju Amamjirionwu <a.karoluju97@gmail.com>
2020-11-18 18:41:03 +0000

    Tag movies to watch in the future

--- src/contexts/moviesContext.js
+++ src/contexts/moviesContext.js
@@ -46,8 +46,8 @@ const MoviesContextProvider = (props) => {
   };
 
   const addToWatchList = (movieId) => {
-    const index = state.movies.map((m) => m.id).indexOf(movieId);
-    dispatch({ type: "add-watchFuture", payload: { movie: state.movies[index] } });
+    const index = state.upocoming.map((m) => m.id).indexOf(movieId);
+    dispatch({ type: "add-watchList", payload: { movie: state.upcoming[index] } });
   };
 
   const addReview = (movie, review) => {
